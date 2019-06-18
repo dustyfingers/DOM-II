33034fc0bbbbc3b9d60a45a303cb14730d9aff68
dustyfingers <dustyfingers01257@gmail.com>
2019-06-18 15:55:43 -0600

    added ten event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+let navbar = document.querySelector('.main-navigation');
+let busPic = document.getElementById('bus-img');
+let btns = document.querySelectorAll('.btn');
+let firstArticlePic = document.querySelector('.img-content');
+let footer = document.querySelector('.footer');
+let contentSections = document.querySelectorAll('.content-section');
+let logo = document.querySelector('.logo-heading');
+
+navbar.addEventListener('click', evt => { evt.target.style.display = 'none' });
+busPic.addEventListener('mouseover', evt => { evt.target.style.border = '2px solid red' });
+btns.forEach(btn => {
+    btn.addEventListener('dblclick', evt => { 
+        evt.target.style.backgroundColor = 'limegreen';
+        evt.target.style.color = 'black';
+     });
+});
+firstArticlePic.addEventListener('click', evt => { navbar.style.display = 'block' });
+footer.addEventListener('mouseover', evt => { evt.target.style.background = 'purple' });
+contentSections.forEach(section =>{
+    section.addEventListener('click', evt => { 
+        evt.target.style.background = 'orange';
+        evt.target.style.color = 'white';
+     });
+});
+logo.addEventListener('click', evt => { evt.target.style.color = 'limegreen' });
+
